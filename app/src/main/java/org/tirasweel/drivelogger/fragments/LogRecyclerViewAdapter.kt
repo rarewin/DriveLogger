09254f60deecac98cc7e158e29e692596b3cbf82
Katsuki Kobayashi <rare@tirasweel.org>
2022-08-07 23:33:43 +0900

    delete an invalid fragment, add a fab for adding a new log

--- app/src/main/java/org/tirasweel/drivelogger/fragments/LogRecyclerViewAdapter.kt
+++ app/src/main/java/org/tirasweel/drivelogger/fragments/LogRecyclerViewAdapter.kt
@@ -3,10 +3,9 @@ package org.tirasweel.drivelogger.fragments
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import io.realm.kotlin.query.RealmResults
 
-import org.tirasweel.drivelogger.databinding.FragmentLogBinding
+import org.tirasweel.drivelogger.databinding.FragmentLogListBinding
 import org.tirasweel.drivelogger.db.DriveLog
 
 class LogRecyclerViewAdapter(
@@ -16,7 +15,7 @@ class LogRecyclerViewAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         return ViewHolder(
-            FragmentLogBinding.inflate(
+            FragmentLogListBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
@@ -26,19 +25,11 @@ class LogRecyclerViewAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-        holder.idView.text = item.id.toString()
     }
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(binding: FragmentLogBinding) : RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
-
-        override fun toString(): String {
-            return super.toString() + " '${contentView.text}'"
-        }
+    inner class ViewHolder(binding: FragmentLogListBinding) : RecyclerView.ViewHolder(binding.root) {
     }
 
 }
