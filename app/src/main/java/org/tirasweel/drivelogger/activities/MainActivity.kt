13d54f018984ce99159bd707c152b12ac4facc63
Katsuki Kobayashi <rare@tirasweel.org>
2022-08-07 13:42:12 +0900

    add MainActivity, and a fragment for the activity

--- app/src/main/java/org/tirasweel/drivelogger/activities/MainActivity.kt
+++ app/src/main/java/org/tirasweel/drivelogger/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package org.tirasweel.drivelogger
+package org.tirasweel.drivelogger.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.tirasweel.drivelogger.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
