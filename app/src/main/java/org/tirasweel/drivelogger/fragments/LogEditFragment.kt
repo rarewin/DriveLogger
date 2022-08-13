5d5725ae57067b3251375bfebfce84a995029994
Katsuki Kobayashi <katsuki.kobayashi@xacti-co.com>
2022-08-13 17:09:20 +0900

    WIP: add menu to a toolbar

--- app/src/main/java/org/tirasweel/drivelogger/fragments/LogEditFragment.kt
+++ app/src/main/java/org/tirasweel/drivelogger/fragments/LogEditFragment.kt
@@ -1,18 +1,38 @@
 package org.tirasweel.drivelogger.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import org.tirasweel.drivelogger.R
+import org.tirasweel.drivelogger.databinding.FragmentLogEditBinding
 
 class LogEditFragment : Fragment() {
+
+    private var binding: FragmentLogEditBinding? = null
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_log_edit, container, false)
+
+        binding = FragmentLogEditBinding.inflate(inflater, container, false)
+
+        binding?.toolbar?.apply {
+            inflateMenu(R.menu.menu_items)
+
+            menu.findItem(R.id.menu_register_log).apply {
+                isVisible = true
+            }
+        }
+
+        return binding?.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
