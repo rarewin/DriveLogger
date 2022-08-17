5eae764001ab212a2eb347b2cdc80cdad1c5408a
Katsuki Kobayashi <katsuki.kobayashi@xacti-co.com>
2022-08-18 08:15:04 +0900

    ログ編集画面用フラグメントを追加

--- app/src/main/java/org/tirasweel/drivelogger/db/DriveLog.kt
+++ app/src/main/java/org/tirasweel/drivelogger/db/DriveLog.kt
@@ -4,10 +4,54 @@ import io.realm.kotlin.types.ObjectId
 import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.Index
 
-class DriveLog: RealmObject {
+//enum class MileageUnit : RealmObject {
+//    KiloMeter,
+//    Mile;
+//
+//    var enum: MileageUnit
+//        get() {
+//            return valueOf(enumDescription)
+//        }
+//        set(newValue) {
+//            enumDescription = newValue.name
+//        }
+//    private var enumDescription: String = MileageUnit.KiloMeter.name
+//}
+
+class DriveLog : RealmObject {
     /**
      * ID
      */
     @Index
     var id: ObjectId = ObjectId.create()
+
+    /**
+     * 作成日
+     */
+    var createdDate: Long = 0
+
+    /**
+     * 更新日
+     */
+    var updatedDate: Long = 0
+
+    /**
+     * 日付
+     */
+    var date: Long = 0
+
+    /**
+     * 走行距離x1,000
+     */
+    var milliMileage: Long = 0
+
+    /**
+     * 距離の単位
+     */
+    // var mileageUnit: MileageUnit = MileageUnit.KiloMeter
+
+    /**
+     * メモ
+     */
+    var memo: String = ""
 }
--- app/src/main/java/org/tirasweel/drivelogger/fragments/LogEditFragment.kt
+++ app/src/main/java/org/tirasweel/drivelogger/fragments/LogEditFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import org.tirasweel.drivelogger.R
@@ -36,7 +37,21 @@ class LogEditFragment : Fragment() {
                 isVisible = true
                 isEnabled = true
             }
+
+            setOnMenuItemClickListener { item ->
+                when (item?.itemId) {
+                    R.id.menu_register_log -> {
+                        Toast.makeText(this.context, "TEST", Toast.LENGTH_LONG).show()
+                    }
+                    else -> {
+                        throw IllegalStateException("$item is unexpected here")
+                    }
+                }
+
+                true
+            }
         }
+
 
         return binding?.root
     }
