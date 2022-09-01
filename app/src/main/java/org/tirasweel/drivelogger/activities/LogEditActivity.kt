4b8d9f1185af5edc6be78543dfe5829ef8e42a9f
Katsuki Kobayashi <katsuki.kobayashi@xacti-co.com>
2022-09-01 11:27:49 +0900

    Intentにパラメーターを追加

--- app/src/main/java/org/tirasweel/drivelogger/activities/LogEditActivity.kt
+++ app/src/main/java/org/tirasweel/drivelogger/activities/LogEditActivity.kt
@@ -1,13 +1,26 @@
 package org.tirasweel.drivelogger.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import org.tirasweel.drivelogger.BuildConfig
 import org.tirasweel.drivelogger.R
 import org.tirasweel.drivelogger.databinding.ActivityLogEditBinding
 import org.tirasweel.drivelogger.fragments.LogEditFragment
 
 class LogEditActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG: String =
+            "${BuildConfig.APPLICATION_ID}.LogEditActivity"
+    }
+
+    enum class IntentKey {
+        OpenMode,
+        Date,
+        Mileage
+    }
 
     private lateinit var binding: ActivityLogEditBinding
 
@@ -16,12 +29,24 @@ class LogEditActivity : AppCompatActivity() {
 
         binding = ActivityLogEditBinding.inflate(layoutInflater)
 
+        val bundle = intent.extras
+
+        val mode = bundle?.get(IntentKey.OpenMode.name) as LogEditFragment.OpenMode?
+        val date = bundle?.getLong(IntentKey.Date.name)
+        val mileage = bundle?.getLong(IntentKey.Mileage.name)
+
+        Log.d(TAG, "mode: $mode, date: $date, mileage: $mileage")
+
+        if (mode == null) {
+            TODO("Not yet implemented (OpenMode is required)")
+        }
+
         setContentView(binding.root)
 
         supportFragmentManager.commit {
             add(
                 R.id.logEditFragmentContainer,
-                LogEditFragment.newInstance(LogEditFragment.OpenMode.Update)
+                LogEditFragment.newInstance(mode)
             )
         }
     }
--- app/src/main/java/org/tirasweel/drivelogger/activities/MainActivity.kt
+++ app/src/main/java/org/tirasweel/drivelogger/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.tirasweel.drivelogger.databinding.ActivityMainBinding
 import org.tirasweel.drivelogger.db.DriveLog
+import org.tirasweel.drivelogger.fragments.LogEditFragment
 import org.tirasweel.drivelogger.fragments.LogListFragment
 
 class MainActivity : AppCompatActivity(),
@@ -19,14 +20,23 @@ class MainActivity : AppCompatActivity(),
 
         setContentView(binding.root)
 
-        binding.fabNewDriveLog.setOnClickListener { view ->
-            val intent = Intent(this@MainActivity, LogEditActivity::class.java)
+        binding.fabNewDriveLog.setOnClickListener { _ ->
+            val intent = Intent(this@MainActivity, LogEditActivity::class.java).apply {
+                putExtra(LogEditActivity.IntentKey.OpenMode.name, LogEditFragment.OpenMode.New)
+            }
 
             startActivity(intent)
         }
     }
 
     override fun onItemClick(log: DriveLog) {
-        TODO("Not yet implemented")
+
+        val intent = Intent(this@MainActivity, LogEditActivity::class.java).apply {
+            putExtra(LogEditActivity.IntentKey.OpenMode.name, LogEditFragment.OpenMode.Update)
+            putExtra(LogEditActivity.IntentKey.Date.name, log.date)
+            putExtra(LogEditActivity.IntentKey.Mileage.name, log.milliMileage)
+        }
+
+        startActivity(intent)
     }
 }
